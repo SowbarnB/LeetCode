7edbdfbaa41d8a054dc648521618b6b9f4c564f7
SowbarnB <117518754+SowbarnB@users.noreply.github.com>
2024-03-14 19:07:12 +0530

    Time: 1 ms (99.85%) | Memory: 46.2 MB (26.15%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+}
