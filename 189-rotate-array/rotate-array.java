e0f003b606bfde4b28e9e67ae5edd0bc6cc04037
SowbarnB <117518754+SowbarnB@users.noreply.github.com>
2024-03-16 22:15:00 +0530

    Time: 1 ms (49.27%) | Memory: 57.3 MB (59.74%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -7,15 +7,15 @@ class Solution {
         int[] temp = new int[k];
 
         for (int i = 0; i < k; i++) {
-            temp[i] = arr[n - k + i];
+            temp[i] = arr[n - k + i]; //[5, 6, 7]
         }
 
         for (int i = n - 1; i >= k; i--) {
-            arr[i] = arr[i - k];
+            arr[i] = arr[i - k]; //[1,2,3,1,2,3,4]
         }
 
         for (int i = 0; i < k; i++) {
-            arr[i] = temp[i];
+            arr[i] = temp[i]; //[5, 6, 7, 1, 2, 3, 4]
         }
     }
 }
