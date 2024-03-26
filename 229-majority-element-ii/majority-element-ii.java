4edef246050689d903b6fa45c012a5d352db3d32
SowbarnB <117518754+SowbarnB@users.noreply.github.com>
2024-03-26 19:10:01 +0530

    Time: 3 ms (75.07%) | Memory: 47.9 MB (46.73%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -5,19 +5,19 @@ class Solution {
 
         for(int i = 0; i < n; i++)
         {
-            if(ls.size() == 0 || ls.get(0) != nums[i])
+            if(ls.size() == 0 || ls.get(0) != nums[i]) //ls 3  
             {
                 int count = 0;
                 for(int j = 0; j < n; j++)
                 {
                     if(nums[j] == nums[i])
                     {
-                        count++;
+                        count++;  //1 
                     }
                 }
                 if(count > (n/3))
                 {
-                    ls.add(nums[i]);
+                    ls.add(nums[i]);  //3 
                 }
             }
             if(ls.size() == 2) break;
