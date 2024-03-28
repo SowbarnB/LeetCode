822010b66e4a4d20f5bcf19bc2b24f3ad9e7cd2a
SowbarnB <117518754+SowbarnB@users.noreply.github.com>
2024-03-28 15:57:08 +0530

    Time: 0 ms (100.00%) | Memory: 50.4 MB (37.52%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,18 +1,24 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
-
         if(n == 1) return nums[0];
-        
-        for(int i = 0; i < n; i++) {
-            if(i == 0) {
-                if(nums[i] != nums[i + 1]) return nums[i];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        int low = 1, high = n - 2;
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid - 1]) return nums[mid];
+
+            if((mid % 2 == 1 && nums[mid] == nums[mid - 1]) 
+            || (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+            {
+                low = mid + 1;
             }
-            else if(i == n - 1) {
-                if(nums[i] != nums[i - 1]) return nums[i];
-            }
-            else {
-                if(nums[i] != nums[i + 1] && nums[i] != nums[i - 1]) return nums[i];
+            else
+            {
+                high = mid - 1;
             }
         }
         return -1;
