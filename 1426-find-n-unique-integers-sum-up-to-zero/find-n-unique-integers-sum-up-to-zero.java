3436aed9498fc480ff1d0b6d81c2884bbf2d5804
SowbarnB <117518754+SowbarnB@users.noreply.github.com>
2024-03-30 22:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (48.88%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -1,22 +1,10 @@
 class Solution {
     public int[] sumZero(int n) {
-        int mid = -(n / 2);
         int arr[] = new int[n];
-
-        if (n % 2 != 0) {
-            for (int i = 0; i < n; i++) {
-                arr[i] = mid++;
-            }
-        }
-        else {
-            for (int i = 0; i < n; i++){
-                if (mid == 0){
-                    mid++;
-                    arr[i] = mid++;
-                    continue;
-                }
-                arr[i] = mid++;
-            }
+        
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = 2 * i - n + 1;
         }
         return arr;
     }
