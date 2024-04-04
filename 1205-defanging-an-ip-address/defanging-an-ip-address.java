c90b6e54b12cb2ec85552b5fe7bc2daf00395467
SowbarnB <117518754+SowbarnB@users.noreply.github.com>
2024-04-04 22:06:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (97.14%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -1,6 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String defange = address.replace(".", "[.]");
-        return defange;
+        return address.replace(".", "[.]");
     }
 }
