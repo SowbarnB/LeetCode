41336c1d03cbb309a236a4d8afb6e3292b6f0811
SowbarnB <117518754+SowbarnB@users.noreply.github.com>
2024-04-06 21:11:20 +0530

    Time: 10 ms (93.76%) | Memory: 45.4 MB (41.40%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        int open = 0, close = 0;
+
+        for (char ch : s.toCharArray()) 
+        {
+            if (ch == '(') 
+            {
+                open++;
+            } else if (ch == ')') 
+            {
+                if (open > close) 
+                {
+                    close++;
+                }
+            }
+        }
+        open = close;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray()) 
+        {
+            if (ch == '(') 
+            {
+                if (open > 0) 
+                {
+                    sb.append(ch);
+                    open--;
+                }
+            } 
+            else if (ch == ')') 
+            {
+                if (open < close) 
+                {
+                    sb.append(ch);
+                    close--;
+                }
+            } 
+            else 
+            {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
