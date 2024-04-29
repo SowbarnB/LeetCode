bc5d97b7a8b9b920c4abb46edbc971d1da427284
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-04-29 17:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (61.57%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int tribonacci(int n) {
+        int t0 = 0, t1 = 1, t2 = t0 + t1, T = 0;
+        if(n == 0)
+        {
+            return 0;
+        }
+        else if(n == 1 || n == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            for(int i = 2; i < n; i++)
+            {
+                T = t0 + t1 + t2; // 2
+                t0 = t1;
+                t1 = t2;
+                t2 = T;
+            }
+        }
+        return T;
+    }
+}
