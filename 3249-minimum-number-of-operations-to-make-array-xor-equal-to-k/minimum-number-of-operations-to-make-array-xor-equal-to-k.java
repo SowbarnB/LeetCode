3d6a2d04a82b55eee75f462b8653e2bf5e697c98
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-04-30 17:23:57 +0530

    Time: 1 ms (100.00%) | Memory: 58.4 MB (13.16%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -4,17 +4,17 @@ class Solution {
 
         for(int num : nums)
         {
-            result = result ^ num;
+            result = result ^ num; //2 //3 //0  //4
         }
         int ans = 0;
         while(k > 0 || result > 0)
         {
-            if((k & 1) != (result & 1))
+            if((k & 1) != (result & 1)) //(1,0) (0,0) (0,1)
             {
-                ans++;
+                ans++;  //1 //2
             }
-            k >>= 1;
-            result >>= 1;
+            k >>= 1; //0000  
+            result >>= 1; //0010 //0001 //0000
         }
     
         return ans;
