7d9a7b41491d32169e8945d182b3e9da9dde2ba1
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-05-14 23:57:45 +0530

    Time: 102 ms (17.02%) | Memory: 41.2 MB (35.87%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -0,0 +1,34 @@
+class Solution {
+public int getMaximumGold(int[][] grid) {
+int ans = 0;
+for(int i=0; i<grid.length; i++){
+for(int j=0; j<grid[0].length; j++){
+if(grid[i][j]!=0){
+ans = Math.max(ans, helper(grid, i, j, new boolean[grid.length][grid[0].length]));
+}
+}
+}
+return ans;
+}
+
+int[] x = {-1, 0, 0, 1};
+int[] y = {0, -1, 1, 0};
+
+int helper(int[][] grid, int row, int col, boolean[][] visited){
+if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] || grid[row][col] == 0) {
+return 0;
+}
+
+visited[row][col] = true;
+int totalGold = grid[row][col];
+
+for(int i=0; i<4; i++){
+int newX = x[i] + row;
+int newY = y[i] + col;
+totalGold = Math.max(totalGold, grid[row][col] + helper(grid, newX, newY, visited));
+}
+
+visited[row][col] = false; // Reset visited for backtracking
+return totalGold;
+}
+}
