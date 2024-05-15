05dd8d00a6764761d653a3092b039017624a7faf
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-05-15 23:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (67.15%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        
+
+        int arr[] = new int[n * 2];
+
+        int j = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            arr[j++] = nums[i];
+            arr[j++] = nums[n + i];
+        }
+        return arr;
+    }
+}
+//timecomplexity: O(n)
