e75d64a39eda77855977e8765892c7012e075794
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-06-28 12:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (21.21%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,59 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = -1, last = -1;
+        int first = firstOccurence(nums, target);
+        if(first == -1) return new int []{-1, -1};
+        int last = lastOccurence(nums, target);
+        return new int []{first, last};
+    }
+    public int firstOccurence(int [] nums, int target)
+    {
+        int first = -1;
+        int n = nums.length;
+        int low = 0, high = n - 1;
 
-        for(int i = 0; i < nums.length; i++)
+        while(low <= high)
         {
-            if(nums[i] == target)
+            int mid = low + (high - low) /2;
+            if(nums[mid] == target)
             {
-                if(first == -1)
-                {
-                    first = i;
-                }
-                last = i;
+                first = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
             }
         }
-        return new int[]{first, last};
+        return first;
+    }
+    public int lastOccurence(int nums[], int target)
+    {
+        int last = -1;
+        int n = nums.length;
+        int low = 0, high = n - 1;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target)
+            {
+                last = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return last;
     }
 }
-//timecomplexity: O(n)
+//timecomplexity: O(log n)
