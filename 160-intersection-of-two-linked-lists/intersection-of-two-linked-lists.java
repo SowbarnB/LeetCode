375546e8dbbe1ce9ffc1eee21334839be8a78101
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-07-15 17:40:23 +0530

    Time: 1 ms (99.87%) | Memory: 48.5 MB (49.34%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,46 +11,23 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        if(headA == null || headB == null) return headA;
+
         ListNode t1 = headA;
         ListNode t2 = headB;
-
-        int N1 = 0;
-        while(t1 != null)
-        {
-            N1++;
-            t1 = t1.next;
-        }
-
-        int N2 = 0;
-        while(t2 != null)
-        {
-            N2++;
-            t2 = t2.next;
-        }
-        
-        if(N1 < N2)
-        {
-            return collisionpoint(headA, headB, N2 - N1);
-        }
-        else
-        {
-            return collisionpoint(headB, headA, N1 - N2);
-        }
-    }
-    public ListNode collisionpoint(ListNode t1, ListNode t2, int d)
-    {
-        while(d > 0)
-        {
-            d--;
-            t2 = t2.next;
-        }
 
         while(t1 != t2)
         {
             t1 = t1.next;
             t2 = t2.next;
+
+            if(t1 == t2) return t1;
+
+            if(t1 == null) t1 = headB;
+            if(t2 == null) t2 = headA;
         }
         return t1;
     }
 }
-//timecomplexity: O(n + 2m)
+//timecomplexity: O(n1 + n2)
