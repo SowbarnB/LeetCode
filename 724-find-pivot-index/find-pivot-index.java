273fb6c86fa3056f63ac139f2296e25b5df0f3c4
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-10-07 23:25:02 +0530

    Time: 171 ms (9.39%) | Memory: 45.3 MB (68.46%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+        int n = nums.length;
+
+        int prefixSum[] = new int[n];
+        int suffixSum[] = new int[n];
+
+        //finding the prefixSum array
+        prefixSum[0] = nums[0];
+        for(int i=1; i<n; i++)
+        {
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+            System.out.print(prefixSum[i] + " ");
+        }
+
+        //finding the suffixSum array
+        suffixSum[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--)
+        {
+            suffixSum[i] = suffixSum[i+1] + nums[i];
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(prefixSum[i]==suffixSum[i])
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
