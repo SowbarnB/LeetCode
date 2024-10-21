500c1e652791dd151c43403672b675461805411d
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-10-21 15:08:33 +0530

    Time: 1 ms (95.04%) | Memory: 44.3 MB (67.16%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,25 +2,25 @@ class Solution {
     List<List<Integer>> result = new ArrayList<>();
     void perm(int[] nums,int f,int n)
     {
-        if(f==n)
+        if(f == n)
         {
             List<Integer> curr = new ArrayList<>();
-            for(int i:nums)
+            for(int i : nums)
             {
                 curr.add(i);
             }
             result.add(curr);
             return;
         }
-        for(int i=f;i<=n;i++)
+        for(int i = f; i <= n; i++)
         {
-            if(i != f) swap(nums,i,f);
-            perm(nums,f+1,n);
-            if(i != f) swap(nums,i,f);
+            if(i != f) swap(nums, i, f);
+            perm(nums, f + 1, n);
+            if(i != f) swap(nums, i, f);
         }
     }
 
-    void swap(int[] arr,int i,int j)
+    void swap(int arr[], int i, int j)
     {
         int temp = arr[i];
         arr[i] = arr[j];
@@ -28,7 +28,7 @@ class Solution {
     }
     public List<List<Integer>> permute(int[] nums) 
     {
-        perm(nums,0,nums.length-1);
+        perm(nums, 0, nums.length-1);
         return result;
     }
 }
