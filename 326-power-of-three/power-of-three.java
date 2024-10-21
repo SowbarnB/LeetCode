af7ad252444b72ee269433b6cbff85e7b0133346
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-10-21 10:24:45 +0530

    Time: 8 ms (90.39%) | Memory: 44.3 MB (19.91%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,16 +1,16 @@
 class Solution {
-    public boolean isPowerOfThree(int n) {
-        if (n == 1) {
-            return true;
-        } else if (n == 0) {
+    public boolean isPowerOfThree(int n) 
+    {
+         if (n <= 0) {
             return false;
-        } else {
+        }
+        while (n > 1) {
             if (n % 3 != 0) {
                 return false;
             }
-            return isPowerOfThree(n / 3);
+            n = n / 3;
         }
+        return true;
+        
     }
 }
-//timecomplexity: O(log n)
-//spacecomplexity: O(1)
