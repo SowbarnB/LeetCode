dcfc0eb3a97392307aa80bc3f527cba71a9ac071
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-10-21 09:32:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (19.61%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        
+        int n = sentence.length();
+
+        for(char ch = 'a'; ch <= 'z'; ch++)
+        {
+            if(!sentence.contains(String.valueOf(ch)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
