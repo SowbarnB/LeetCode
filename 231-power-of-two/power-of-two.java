de28560a6390a1e128c9dc58d730c47d8daf1222
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2024-10-21 10:03:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (51.78%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return (n > 0) && ((n & (n - 1)) == 0);
-        //n = 1            1 & (0) == 0  //true
-        //n = 16           16 & (15) = 10000 & (01111) == 0 //true
+        if(n == 0 || 1073741824 % n != 0 || n < 0 ) return false;
+
+        return true;
     }
 }
-//timecomplexity: O(1);
