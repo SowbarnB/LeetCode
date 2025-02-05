c7983eaf0349129ae4ae335b58d7b9eb26a85173
Sowbarn B <117518754+SowbarnB@users.noreply.github.com>
2025-02-05 11:46:51 +0530

    Time: 2 ms (41.24%) | Memory: 44.6 MB (64.29%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,36 +1,27 @@
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
-    void perm(int[] nums,int f,int n)
-    {
-        if(f == n)
-        {
-            List<Integer> curr = new ArrayList<>();
-            for(int i : nums)
-            {
-                curr.add(i);
-            }
-            result.add(curr);
-            return;
-        }
-        for(int i = f; i <= n; i++)
-        {
-            if(i != f) swap(nums, i, f);
-            perm(nums, f + 1, n);
-            if(i != f) swap(nums, i, f);
-        }
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> resultList = new ArrayList<>();
+        backtract(resultList, new ArrayList<>(), nums);
+        return resultList;
+
     }
 
-    void swap(int arr[], int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public List<List<Integer>> permute(int[] nums) 
-    {
-        perm(nums, 0, nums.length-1);
-        return result;
+    private void backtract(List<List<Integer>> resultList, ArrayList<Integer> tempList, int[] nums) {
+
+        if (tempList.size() == nums.length) {
+            resultList.add(new ArrayList<>(tempList));
+            return;
+        }
+        for (int number : nums) {
+
+            if (tempList.contains(number)) {
+                continue;
+            }
+            tempList.add(number);
+            backtract(resultList, tempList, nums);
+            tempList.remove(tempList.size() - 1);
+
+        }
     }
 }
-//timecomplexity: O(n!)
-//spacecomplexity: O(n!)
